db3b74aced0df1bbab7412a65c5e12b1a73b5c75
Cody Ko In A Box <codykoinabox@gmail.com>
2023-06-21 02:00:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) =>{
-    res.send({'message': 'Hello, World!'})
+    res.send('Hello, World!')
 })
 
 const PORT = process.env.PORT || 8080
